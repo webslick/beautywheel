b1089286a6a5ad1a3bb93c9662eee2f321962d9d
Slick <cnbv170291@yandex.ru>
2024-01-20 21:57:55 +0700

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -62,9 +62,9 @@ const {tg} = useTelegram()
     
     // tg.MainButton.text = "GUGA";
   
-    useCallback(()=>{
-      // tg.sendData(JSON.stringify({userinfo: tg.initDataUnsafe.user, prize: prizeNumber})); 
-    },[])
+    // useCallback(()=>{
+    //   tg.sendData(JSON.stringify({userinfo: tg.initDataUnsafe.user, prize: prizeNumber})); 
+    // },[])
     console.log("stoppppppppp");
     setMustSpin(false);
   }
